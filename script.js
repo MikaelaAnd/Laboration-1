7c036ee110ac898c6bdb66d99d80281b0712084d
Mikaela Andersson <mike@Mikaelas-MacBook-Pro.local>
2020-10-26 12:03:38 +0100

    added first question and html file

---
+++ script.js
@@ -0,0 +1,3 @@
+function startGame() {
+    alert("Hello Player One! Welcome to Atlas Ski-resort. You have now jumped out of the lift, standing on top of the ski mountain. Which lane do you want to shred down on?")
+}
