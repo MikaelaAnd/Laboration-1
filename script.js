e4c56e95d6405929f612c080f1981594cedb1c35
Mikaela Andersson <mike@Mikaelas-MacBook-Pro.local>
2020-10-26 12:31:16 +0100

    added pickALane function

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function startGame() {
     alert("Hello Player One! Welcome to Atlas Ski-resort. You have now jumped out of the lift, standing on top of the ski mountain. Which lane do you want to shred down on?")
 }
+
+function pickALane() {
+    var text;
+    var decideLane = prompt("Which lane? Red lane or Green Lane?");
+    switch(decideLane) {
+        case "Red Lane":
+            text = "Great! Head down to the red lane!";
+            break;
+        case "Green Lane":
+            text = "Good choice! Head down to the green lane!";
+            break;
+        default:
+            text = "Pick again!";
+            break;
+
+    }
+}
+console.log(pickALane);
