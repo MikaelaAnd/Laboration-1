ecf735d7af84b1613bf5f41e058560ca60628dab
Mikaela Andersson <mikaelaaand@gmail.com>
2020-10-30 10:01:08 +0100

    spell-controll

--- script.js
+++ script.js
@@ -7,54 +7,54 @@ let currentScene = 0
 // The scenes of the game
 const scenes = [
     { // Start 0
-        description: "Hello Player One! Welcome to Atlas Ski-resort. You have now jumped out of the lift, standing on top of the ski mountain. Which lane do you want to shred down on? Write 'Red lane' or 'Green lane'",
+        description: "Hello Player One! Welcome to Atlas Ski-resort. You have now jumped out of the lift, standing on top of the snowy mountain. Which lane do you want to shred down on? Write 'Green lane' or 'Red lane'",
         choices: ["Green lane", "Red lane"],
         nextScene: [2, 1]
     }, 
     //FIRST EVENT
     { //Red lane 1
-        description: "Alright Player one! Shredding down the red lane, you are now facing a big jump! Do you want to jump? Write 'Yes' or 'No'",
+        description: "Alright Player One! Shredding down the red lane, you are now facing a big jump! Do you want to jump? Write 'Yes' or 'No'",
         choices: ["Yes", "No"],
         nextScene: [4, 3]
     },
     { //Green lane 2
-        description: "You chose the green lane! Whilst shredding down you are looking around and the view is incredible! Do you want to stop and take a pic to your instagram? Write Yes or No",
+        description: "You chose the green lane! Whilst shredding down you are looking around and the view is incredible! Do you want to stop and take a pic to your instagram? Write 'Yes' or 'No'",
         choices: ["Yes", "No"],
         nextScene: [5, 3]
     },
     //EVENT WHEN PLAYER CHOSED 'NO'
     { // Both lanes 3
-        description: "You decided to kept on shredding down the mouintain without a care in the world, you are now down by the lift. Do you want to go again? Write 'Yes' or 'No'",
+        description: "You decided to keep shredding down the mountain without a care in the world, you are now down by the lift. Do you want to go again? Write 'Yes' or 'No'",
         choices: ["Yes", "No"],
         nextScene: [0, 10]
     },
     //SECOND EVENT 2
     { // Red lane 4
-        description: "Wow! You are actually flying Player one! But something is happening with your left ski-boot. Help! If you want to try to turn in the sky write Try, if you want to hope for the best and just land, write Land. ",
+        description: "Wow! You are actually flying Player One! But something is happening with your left ski-boot. Help! If you want to try to turn to maybe fix it, write 'Try', if you are hoping for the best and just try to land, write 'Land'. ",
         choices: ["Try", "Land"],
         nextScene: [6, 8]
     },
     { // Green lane 5
-        description: "Argh! Your phone is not acclimatized with the cold weather, you've only got 1% battery left. Ok Player one, you got two options: Write Rush if you want to rush down to the after-ski and charge your phone or write Calm if you don't care about the battery and just wanna enjoy the skiing down to the lift again",
+        description: "Argh! Your phone is not acclimatized with the cold weather, you've only got 1% battery left. Ok Player One, you've got two options: Write 'Rush' if you want to rush down to the after-ski and charge your phone or write 'Calm' if you don't care about the battery and just wanna enjoy the skiing down to the lift again",
         choices: ["Rush", "Calm"],
         nextScene: [7, 9]
     },
     //HOSPITAL ENDINGS 
     { // Red lane 6
-        description: "You chose to turn! Sadly, you twist so much so you loose balance and land on your knee. Aouch! But no need to worry, Atlas help-scooter is on it's way and will take you to the hospital. You won't be able to ski anymore today! Write Ok and we will se you tomorrow!",
+        description: "You chose to turn! Sadly, you twist so much so you loose balance and land on your knee. Aouch! But no need to worry, Atlas help-scooter is on it's way and will take you to the hospital. You won't be able to ski anymore today! Write 'Ok' and we will se you tomorrow!",
         choices: ["Ok"],
         nextScene: []
         
     },
     { // Green lane 7
-        description: "You chose to rush down the mountain. Not a super idea, in all the white snow you miss to notice a rock and you fall and land flat on your nouse. Aouch! But no need to worry, Atlas help-scooter is on it's way. You won' be able to ski anymore today! Write Ok and we will see you tomorrow!",
+        description: "You chose to rush down the mountain. Not a super idea, you miss to notice a rock in then white snow, you fall and land flat on your nose. Aouch! But no need to worry, Atlas help-scooter is on it's way. You won' be able to ski anymore today! Write 'Ok' and we will see you tomorrow!",
         choices: ["Ok"],
         nextScene: []
 
     },
     //ENDING
     { // Red lane 8
-        description: "You landed perfectly, you ski-boot was just playing a trick in the wind! You swoosh pass a couple of admirers and you are now in the end of the mountain. Do you want to go again, write 'Yes' or 'No'",
+        description: "You landed perfectly, your ski-boot was just playing a trick in the wind! You swoosh pass a couple of admirers and you are now in the end of the mountain. Do you want to go again? Write 'Yes' or 'No'",
         choices: ["Yes", "No"],
         nextScene: [0, 10]
     },
@@ -64,7 +64,7 @@ const scenes = [
         nextScene: [0, 10]
     },
     { // AFTER-SKI 10
-        description: "Good job Player One! You can now grab a well deserved beer fom the after-ski, but remember to keep distance and no dancing on the tables. Write 'Ok' and Stay safe!",
+        description: "Good job Player One! You can now grab a well deserved beer from the after-ski, but remember to keep distance and no dancing on the tables. Write 'Ok' and Stay safe!",
         choices: ["Ok"],
         nextScene: []
         
